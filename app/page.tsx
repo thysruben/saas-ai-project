29ed0c3aa7b3e702e65d67800076b8503edecd27
rubenthys22@gmail.com <rubenthys22@gmail.com>
2023-09-08 16:44:24 +0200

    Initialise shadcn ui

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    // <p className="text-6xl text-green-400">Hello world</p>
+    <Button variant="destructive">Click me</Button>
+  )
+}
